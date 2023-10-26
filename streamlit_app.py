e16dded5f14391949949c1ef020467b848e168af
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-26 17:54:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import bitarray
+import os
 
 def create_profile():
     # Get the user's IP address
     ip_address = st.experimental_get_query_params().get("client_ip", [None])[0]
 
     # Check if the user's IP address already has a profile
-    if ip_address and ip_address in profiles:
+    if ip_address and os.path.exists(ip_address):
         st.write("You already have a profile!")
 
     else:
@@ -19,9 +20,18 @@ def create_profile():
             question = st.radio(f"Question {i+1}", ("Yes", "No"))
             configuration.append(question == "Yes")
 
+        # Create a folder for the user's IP address if it doesn't exist
+        if not os.path.exists(ip_address):
+            os.makedirs(ip_address)
+
         # Save the profile and configuration for the user's IP address
-        profiles[ip_address] = {"profile_name": profile_name, "configuration": configuration}
-        with open(f"config_{ip_address}.bin", "wb") as f:
+        profile_path = os.path.join(ip_address, "profile.bin")
+        config_path = os.path.join(ip_address, "config.bin")
+
+        with open(profile_path, "wb") as f:
+            f.write(profile_name.encode())
+
+        with open(config_path, "wb") as f:
             bit_array = bitarray.bitarray(configuration)
             bit_array.tofile(f)
 
@@ -35,30 +45,26 @@ def load_profile():
     ip_address = st.experimental_get_query_params().get("client_ip", [None])[0]
 
     # Check if the user's IP address has a profile
-    if ip_address:
-        try:
-            with open(f"config_{ip_address}.bin", "rb") as f:
-                bit_array = bitarray.bitarray()
-                bit_array.fromfile(f)
-                configuration = list(bit_array)
-                profile_name = profiles[ip_address]["profile_name"]
-                st.write(f"Welcome back, {profile_name}!")
-                st.write("You can load your profile here.")
-                st.write("Profile details:")
-                st.write(f"Profile Name: {profile_name}")
-                st.write(f"Configuration: {configuration}")
-        except FileNotFoundError:
-            st.write("You don't have a profile yet.")
+    if ip_address and os.path.exists(ip_address):
+        profile_path = os.path.join(ip_address, "profile.bin")
+        config_path = os.path.join(ip_address, "config.bin")
+
+        with open(profile_path, "rb") as f:
+            profile_name = f.read().decode()
+
+        with open(config_path, "rb") as f:
+            bit_array = bitarray.bitarray()
+            bit_array.fromfile(f)
+            configuration = list(bit_array)
+
+        st.write(f"Welcome back, {profile_name}!")
+        st.write("You can load your profile here.")
+        st.write("Profile details:")
+        st.write(f"Profile Name: {profile_name}")
+        st.write(f"Configuration: {configuration}")
 
     else:
         st.write("You don't have a profile yet.")
-
-try:
-    profiles = {}
-    with open("config.bin", "rb") as f:
-        profiles = pickle.load(f)
-except FileNotFoundError:
-    pass
 
 def main():
     st.title("Profile Creation")
