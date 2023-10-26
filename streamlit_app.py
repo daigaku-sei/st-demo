9b97584cc05ec7c2fa1c2aac80f9ea5a97740c6b
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-26 17:31:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,40 @@ import streamlit as st
 def main():
     st.sidebar.title("Theme Demo")
     theme = st.sidebar.radio("Select Theme", ("Light", "Dark"))
-    
+
     if theme == "Light":
-        st.set_theme("light")
+        light_theme()
     elif theme == "Dark":
-        st.set_theme("dark")
-    
+        dark_theme()
+
     st.title("Streamlit Theme Demo")
     st.write(f"Selected Theme: {theme}")
+
+def light_theme():
+    st.markdown(
+        """
+        <style>
+        body {
+            color: black;
+            background-color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def dark_theme():
+    st.markdown(
+        """
+        <style>
+        body {
+            color: white;
+            background-color: black;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 if __name__ == "__main__":
     main()
