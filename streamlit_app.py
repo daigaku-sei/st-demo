52c582bd300791da489c56f4ea2ffcaf4dde8f0f
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-26 17:42:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,59 @@
 import streamlit as st
+import bitarray
+
+def create_profile():
+    # Get the user's IP address
+    ip_address = st.experimental_get_query_params().get("client_ip", [None])[0]
+
+    # Check if the user's IP address already has a profile
+    if ip_address and ip_address in profiles:
+        st.write("You already have a profile!")
+
+    else:
+        # Get the profile name from the user
+        profile_name = st.text_input("Enter your profile name")
+
+        # Get the configuration from the user
+        configuration = []
+        for i in range(8):
+            question = st.radio(f"Question {i+1}", ("Yes", "No"))
+            configuration.append(question == "Yes")
+
+        # Save the profile and configuration for the user's IP address
+        profiles[ip_address] = {"profile_name": profile_name, "configuration": configuration}
+        with open(f"config_{ip_address}.bin", "wb") as f:
+            bit_array = bitarray.bitarray(configuration)
+            bit_array.tofile(f)
+
+def load_profile():
+    # Get the user's IP address
+    ip_address = st.experimental_get_query_params().get("client_ip", [None])[0]
+
+    # Check if the user's IP address has a profile
+    if ip_address and ip_address in profiles:
+        profile = profiles[ip_address]
+        st.write(f"Welcome back, {profile['profile_name']}!")
+        st.write("You can load your profile here.")
+
+    else:
+        st.write("You don't have a profile yet.")
+
+try:
+    profiles = {}
+    with open("config.bin", "rb") as f:
+        profiles = pickle.load(f)
+except FileNotFoundError:
+    pass
 
 def main():
-    st.sidebar.title("Theme Demo")
-    theme = st.sidebar.radio("Select Theme", ("Light", "Dark"))
+    st.title("Profile Creation")
 
-    if theme == "Light":
-        light_theme()
-    elif theme == "Dark":
-        dark_theme()
+    option = st.radio("Choose an option", ("Create a new profile", "Load an existing profile"))
 
-    st.title("Streamlit Theme Demo")
-    st.write(f"Selected Theme: {theme}")
-
-def light_theme():
-    st.markdown(
-        """
-        <style>
-        body {
-            color: black;
-            background-color: white;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-def dark_theme():
-    st.markdown(
-        """
-        <style>
-        body {
-            color: white;
-            background-color: black;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    if option == "Create a new profile":
+        create_profile()
+    else:
+        load_profile()
 
 if __name__ == "__main__":
     main()
